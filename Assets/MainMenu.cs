38faebd58ffb536f0920db98306f675a2e0dc180
phonedaran <35362197+phonedaran@users.noreply.github.com>
2018-04-22 20:49:08 +0700

    Add files via upload

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public void PlayGame(){
+		SceneManager.LoadScene("Level 1");   //name scene
+	}
+
+	public void HowToplay(){
+		SceneManager.LoadScene("HowToPlay");  //name scene
+	}
+
+	public void QuitGame(){
+		Debug.Log ("It Quit");  //Check Quit
+		Application.Quit ();
+	}
+
+	public void BackGame(){
+		SceneManager.LoadScene("MainManu");   //name scene
+	}
+
+}
